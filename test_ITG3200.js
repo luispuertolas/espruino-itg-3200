2ca85b2a0357a95f87b539e950e2e9d766d8fcff
luispuertolas <lapb@protonmail.com>
2018-03-19 03:02:11 +0000

    Update test_ITG3200.js

--- test_ITG3200.js
+++ test_ITG3200.js
@@ -1,5 +1,6 @@
+var I2C1 = new I2C();
 I2C1.setup( { scl: B6, sda: B7 } );
-var gyro = require('ITG3200').connect( I2C1 );
+var gyro = require('https://github.com/polhomarkho/espruino-itg-3200/blob/master/ITG3200.js').connect( I2C1 );
 
 console.log("Calibrating offset...");
 
