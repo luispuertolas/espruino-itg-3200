6476d3ff53e00961c00165df6372f374cd3d88b4
luispuertolas <lapb@protonmail.com>
2018-03-26 09:15:38 +0100

    Update ITG3200.js

--- ITG3200.js
+++ ITG3200.js
@@ -24,13 +24,6 @@ ITG3200.prototype.calibrateOffset = function () {
     this.xOffset = (this.xOffset + d.x) / 2;
     this.yOffset = (this.yOffset + d.y) / 2;
     this.zOffset = (this.zOffset + d.z) / 2;
-    if (i % 50 === 0) {
-      console.log("i : " + i);
-      console.log("xOffset : " + this.xOffset);
-      console.log("yOffset : " + this.yOffset);
-      console.log("zOffset : " + this.zOffset);
-      console.log(" ");
-    }
   }
 };
 
